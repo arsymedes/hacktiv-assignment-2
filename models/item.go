156bf68f624bce70ef5a546634ba1609184ec3cf
Ahmad Arsy <arsyahmad14@gmail.com>
2023-10-02 00:21:14 +0700

    Added unique Item Code

--- models/item.go
+++ models/item.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Item struct {
 	gorm.Model
-	ItemCode    string `gorm:"not null;type:varchar(191)" json:"itemcode"`
+	ItemCode    string `gorm:"not null;unique;type:varchar(191)" json:"itemcode"`
 	Description string `gorm:"not null;type:varchar(191)" json:"description"`
 	Quantity    int    `gorm:"not null;type:int" json:"quantity"`
 	OrderID     uint
